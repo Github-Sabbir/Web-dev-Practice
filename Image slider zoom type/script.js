0d8f59eb2aed5c02ea3f11245d39c3bcc7f50b11
Github-Sabbir <mdabdulalimsabbir000@gmail.com>
2026-04-14 23:25:48 +0600

    Image slider zoom type

---
+++ Image slider zoom type/script.js
@@ -0,0 +1,35 @@
+var img_1 = document.getElementById('img_1');
+img_1.onclick = function () {
+    var mainImage = document.getElementById('mainImage');
+    var imgUrl = img_1.getAttribute('src');
+    mainImage.setAttribute('src', imgUrl);
+}
+///////////////////////////////////////////////////////
+var img_2 = document.getElementById('img_2');
+img_2.onclick = function () {
+    var mainImage = document.getElementById('mainImage');
+    var imgUrl = img_2.getAttribute('src');
+    mainImage.setAttribute('src', imgUrl);
+}
+///////////////////////////////////////////////////////
+var img_3 = document.getElementById('img_3');
+img_3.onclick = function () {
+    var mainImage = document.getElementById('mainImage');
+    var imgUrl = img_3.getAttribute('src');
+    mainImage.setAttribute('src', imgUrl);
+}
+///////////////////////////////////////////////////////
+var img_4 = document.getElementById('img_4');
+img_4.onclick = function () {
+    var mainImage = document.getElementById('mainImage');
+    var imgUrl = img_4.getAttribute('src');
+    mainImage.setAttribute('src', imgUrl);
+}
+///////////////////////////////////////////////////////
+var img_5 = document.getElementById('img_5');
+img_5.onclick = function () {
+    var mainImage = document.getElementById('mainImage');
+    var imgUrl = img_5.getAttribute('src');
+    mainImage.setAttribute('src', imgUrl);
+}
+///////////////////////////////////////////////////////
